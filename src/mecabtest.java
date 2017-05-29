8cc1c2f5e26e5e0eaf3858f9b4fadbe7572f6ade
shinsat <kiaorasat@gmail.com>
2017-05-29 23:24:29 +0900

    Initial commit

---
+++ src/mecabtest.java
@@ -0,0 +1,56 @@
+/**
+ * Created by kiaora on 2017/05/29.
+ */
+import org.chasen.mecab.*;
+
+import java.io.File;
+
+public class mecabtest {
+    static {
+        try {
+            File f = new File("/Users/kiaora/deeplearning/mecab-java/libMeCab.so"); // Select libMeCab.so path
+            System.load(f.toString());
+        } catch (UnsatisfiedLinkError e) {
+            System.err.println("Cannot load the example native code.\nMake sure your LD_LIBRARY_PATH contains \'.\'\n" + e);
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] argv) {
+        System.out.println(MeCab.VERSION);
+        Tagger tagger = new Tagger();
+        String str = "太郎は二郎にこの本を渡した。";
+        System.out.println(tagger.parse(str));
+        Node node = tagger.parseToNode(str);
+        for (; node != null; node = node.getNext()) {
+            System.out.println(node.getSurface() + "\t" + node.getFeature());
+        }
+        System.out.println("EOS\n");
+
+        Model model = new Model();
+        Tagger tagger2 = model.createTagger();
+        System.out.println(tagger2.parse(str));
+
+        Lattice lattice = model.createLattice();
+        System.out.println(str);
+        lattice.set_sentence(str);
+        if (tagger2.parse(lattice)) {
+            System.out.println(lattice.toString());
+            for (node = lattice.bos_node(); node != null; node = node.getNext()) {
+                System.out.println(node.getSurface() + "\t" + node.getFeature());
+            }
+            System.out.println("EOS\n");
+        }
+
+        lattice.add_request_type(MeCab.MECAB_NBEST);
+        lattice.set_sentence(str);
+        tagger2.parse(lattice);
+        for (int i = 0; i < 10; ++i) {
+            if (lattice.next()) {
+                System.out.println("nbest:" + i + "\n" +
+                        lattice.toString());
+            }
+        }
+
+    }
+}
